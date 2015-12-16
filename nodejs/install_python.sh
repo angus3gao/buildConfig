dad92bc3af2993c369ccc5f6a3111bb517464b58
angus3gao <gaosi_angus@163.com>
2015-12-16 23:22:31 +0800

    修改install_python脚步，以及添加setuptool github 项目路径

--- nodejs/install_python.sh
+++ nodejs/install_python.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+cd /tmp/
+
 if [ $(id -u) != "0" ]; then
     printf "Error: You must be root to run this tool!\n"
     exit 1
 fi
+
 clear
 wget  http://li.nux.ro/download/nux/dextop/el6/i386/nux-dextop-release-0-2.el6.nux.noarch.rpm 
 rpm -ivh nux-dextop-release-0-2.el6.nux.noarch.rpm
 
-yum -y install python27yum -y install python27
-yum -y install python27-devel
+yum -y install python
+yum -y install python-devel
 
-cd /usr/bin/   rm -rf python   cp python2.7 python 
+cd /usr/bin/   mv python python2.6_back   cp python2.7 python 
 
 python --version
 
 sed -e "s/^#!/usr/bin/python$/#!/usr/bin/python2.6/" /usr/bin/yum
+#https://github.com/jaraco/setuptools/releases
 wget https://enterprise-storage-os.googlecode.com/files/setuptools-0.6c11.tar.gz
 tar xzvf setuptools-0.6c11.tar.gz
 cd setuptools-0.6c11
